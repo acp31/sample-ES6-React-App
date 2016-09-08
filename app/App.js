3d4f3cf07d9006a9cc9e167b403773369f5ecf27
Aaron Phillips <aphillips@johnmcneiltudio.com>
2016-09-08 15:52:33 -0700

    delete function complete

---
+++ app/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import KanbanBoardContainer from './KanbanBoardContainer';
+
+ReactDOM.render(<KanbanBoardContainer />, document.getElementById("root"));
---
+++ app/KanbanBoardContainer.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import KanbanBoard from './KanbanBoard';
+import 'whatwg-fetch';
+import 'babel-polyfill';
+
+const API_URL = 'http://kanbanapi.pro-react.com';
+const API_HEADERS = {
+  'Content-type': 'application/json',
+  Authorization: 'any-string-you-like'
+}
+
+class KanbanBoardContainer extends Component {
+  constructor(){
+    super(...arguments);
+    this.state = {
+      cards: [],
+    };
+  }
+
+  addTask(cardId, taskName){
+
+  }
+  deleteTask(cardId, taskId, taskIndex){
+    // Find the index of the card
+    let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
+
+    // Create a new object without the task
+    let nextState = update(this.state.cards, {
+                            [cardIndex]: {
+                              tasks: {$splice: [[taskIndex,1]] }
+                            }
+                          });
+    // Set the state of the mutated object
+    this.setState({cards:nextState});
+
+    // Call the API to remove the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+      method: 'delete',
+      headers: API_HEADERS
+    });
+  }
+  toggleTask(cardId, taskId, taskIndex){
+
+  }
+
+  componentDidMount(){
+    fetch(API_URL+'/cards', {headers: API_HEADERS})
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({cards: responseData});
+    })
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error)
+    });
+  }
+  render() {
+    return <KanbanBoard cards={this.state.cards}
+                        taskCallbacks={{
+                          toggle: this.toggleTask.bind(this),
+                          delete: this.deleteTask.bind(this),
+                             add: this.addTask.bind(this) }} />
+  }
+}
+
+export default KanbanBoardContainer;
